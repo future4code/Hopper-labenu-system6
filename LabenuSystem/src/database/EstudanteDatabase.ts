fcd6dbd80333f74d6f1bf5a24e017bdbd398d91d
Diego Motta <mottadiego93@gmail.com>
2022-10-07 15:08:49 -0300

    Atualização do projeto

--- LabenuSystem/src/database/EstudanteDatabase.ts
+++ LabenuSystem/src/database/EstudanteDatabase.ts
@@ -2,17 +2,29 @@ import { Estudante } from "../models/Estudante";
 import {BaseDatabase} from  "./BaseDatabase";
 
 export class EstudanteDatabase extends BaseDatabase{
-    TABLE_NAME: "ESTUDANTE";
+    TABLE_NAME = "ESTUDANTE"
 
-    public async getAll(): Promise<Estudante> {
-        return super.getAll()
+
+    public async criarEstudante (estudante: Estudante){
+        await BaseDatabase.connection.raw(`INSERT INTO ${this.TABLE_NAME} 
+        (id, nome, email, data_nasc, turma_id) VALUES(
+            '${estudante.getId()}', 
+            '${estudante.getNome()}', 
+            '${estudante.getEmail()}', 
+            '${estudante.getData_nasc()}', 
+            '${estudante.getTurma_id()}');`)    
     }
 
-    public async create(Estudante: Estudante): Promise<void> {
-        await super.create(Estudante)
+    public async buscarTodosEstudantes(){
+        const resultado = await BaseDatabase.connection.raw(`SELECT * FROM ${this.TABLE_NAME};`)
+        return resultado
+    }
+    public async buscarEstudante(nome: string){
+        const resultado = await BaseDatabase.connection.raw(`SELECT * FROM ${this.TABLE_NAME} WHERE nome LIKE '%${nome}%';`)
+        return resultado
     }
 
-    public async getById(id: string){
-        return super.getById(id)
+    public async mudarEstudante (id: string, turma_id: string){
+        await BaseDatabase.connection.raw(`UPDATE ${this.TABLE_NAME} SET turma_id = '${turma_id}' WHERE id = '${id}';`)
     }
 }
--- LabenuSystem/src/models/Estudante.ts
+++ LabenuSystem/src/models/Estudante.ts
@@ -1,65 +1,7 @@
-export type TEstudante ={
-    id: string,
-    nome: string,
-    email: string,
-    data_nasc: Date,
-    turma_id: string,
-    hoobies: string
-}
+import { Usuario } from "./Usuario";
 
-export class Estudante{
-    constructor(
-        private id: string,
-        private nome: string,
-        private email: string,
-        private data_nasc: Date,
-        private turma_id: string,
-        private hobbies: string
-    ) {
-        this.id = id
-        this.nome = nome
-        this.email = email
-        this.data_nasc = data_nasc
-        this.turma_id = turma_id
-        this.hobbies = hobbies
-    }
-
-    public getId(){
-        return this.id
-    }
-    public getNome(){
-        return this.nome
-    }
-    public getEmail(){
-        return this.email
-    }
-    public getData_nasc(){
-        return this.data_nasc
-    }
-    public getTurma_id(){
-        return this.turma_id
-    }
-    public getHobbies(){
-        return this.hobbies
-    }
-
-    public setId(newId: string){
-        this.id = newId
-    }
-    public setNome(newNome: string){
-        this.nome = newNome
-    }
-    public setEmail(newEmail: string){
-        this.email = newEmail
-    }
-    public setData_nasc(newData_nasc: Date){
-        this.data_nasc = newData_nasc
-        
-    }
-    public setTurma_id(newTurma_id: string){
-        this.turma_id = newTurma_id
-    }
-    public setHobbies(newHobbies: string){
-        this.hobbies = newHobbies
+export class Estudante extends Usuario{
+    constructor(id: string, nome: string, email: string, data_nasc: string, turma_id: string){
+        super(id, nome, email, data_nasc, turma_id)
     }
 }
